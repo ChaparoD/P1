b6e282af57f7916e8324ebeaf484f0ac3138615a
diegoaguayo <dtaguayo@uc.cl>
2022-04-25 17:26:25 -0400

    first approach structs

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -6,4 +6,15 @@ int main(int argc, char const *argv[])
 {
   printf("Hello P1!\n");
   Plane* plano1 = planeInit();
+
+unsigned char buffer[10];
+FILE *ptr;
+
+ptr = fopen("test.bin","rb");  // r for read, b for binary
+
+fread(buffer,sizeof(buffer),1,ptr); 
+for(int i = 0; i<10; i++){
+  printf("%u ", buffer[i]); // prints a series of bytes
+  }
+    
 }
