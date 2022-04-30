695ab0316ada65404bd5fb88dbdac2b6e49dbda9
matumartinez <mtmartinez2@uc.cl>
2022-04-30 16:42:40 -0400

    lifemap ready

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -3,6 +3,55 @@
 #include <string.h> 
 #include "./os_API.h"
 
+#define MAX_SPLIT 255
+#define BUFFER_SIZE 4096
+
+static char **split_by_sep(char *str, char *sep)
+{
+  char **new_str = calloc(MAX_SPLIT, sizeof(char *));
+  int index = 0, len;
+
+  char *token = strtok(str, sep);
+  while (token != NULL)
+  {
+    new_str[index] = calloc(BUFFER_SIZE, sizeof(char));
+    strcpy(new_str[index++], token);
+    token = strtok(NULL, sep);
+  }
+
+  // Remove dangling Windows (\r) and Unix (\n) newlines
+  len = strlen(new_str[index - 1]);
+  if (len > 1 && new_str[index - 1][len - 2] == '\r')
+    new_str[index - 1][len - 2] = '\0';
+  else if (len && new_str[index - 1][len - 1] == '\n')
+    new_str[index - 1][len - 1] = '\0';
+  return new_str;
+}
+
+/*
+ * Reads a line fo user input and returns it as an array of strings
+ */
+char **read_user_input()
+{
+  char *input = calloc(BUFFER_SIZE, sizeof(char));
+  fgets(input, BUFFER_SIZE, stdin);
+  char **split_input = split_by_sep(input, " ");
+  free(input);
+  return split_input;
+}
+
+/*
+ * Frees user input obtained by the read_user_input function
+ */
+void free_user_input(char **input)
+{
+  for (int i = 0; i < MAX_SPLIT; i++)
+  {
+    free(input[i]);
+  }
+  free(input);
+}
+
 int main(int argc, char const *argv[])
 {
   printf("Hello P1!\n");
@@ -10,23 +59,22 @@ int main(int argc, char const *argv[])
   unsigned short buffer[2048]; //unsigned short = 16 bits ( = tamaño de celda (2 Bytes)) Leemos de a páginas
 
   printf("argv[0] = %s\n argv[1] = %s\n ", argv[0], argv[1] );
-  FILE *ptr = NULL;
-  char* diskName = NULL;
-  Page* bitMap = NULL;  //usa una página , entonces trabajamos directamente con la página
   //char* diskName = argv[1];
   //os_mount(diskName, 145363523, &ptr);
   //ptr = fopen(diskName,"rb");
   int iter = 1;
+  char** input;
   while (iter){
-
+    printf("INSERT COMMAND:\n");
+    
+    input = read_user_input();
     // Entra corriendo ./ssdfs os_mount diskName life  (FUNCIONANDO)
-    if (strncmp(argv[2],"os_mount", 15) == 0){
-      printf("Entering OS_MOUNT \n");
-      diskName = argv[2];  //Arroja warning pero funciona
-      unsigned int life = atoi(argv[3]);
-      os_mount(diskName, life, &ptr);
-      bitMap = chargeBitMap(ptr);
-      printBinary(bitMap -> Shells[45]);  //testeado
+    if (strncmp(input[0],"os_mount", 15) == 0){
+      printf("Entering OS_MOUNT\n");
+      char* diskname = argv[1];
+      unsigned int input_life = atoi(input[1]);
+      os_mount(diskname, input_life);
+      printf("Disk Mounted\n");
     }
     //Testing retorno de páginas. //Con esta funcion podemos copiar en nuevos structs Page y Blocks.
     // seekPage(0, 6, ptr, buffer);
@@ -37,31 +85,29 @@ int main(int argc, char const *argv[])
     // }
     
 
-    else if (strncmp(argv[2],"os_bitmap", 15) == 0){
+    else if (strncmp(input[0],"os_bitmap", 15) == 0){
       printf("Entering OS_BITMAP \n");
       //Se asume que ya se montó el disco
-      int num = atoi(argv[3]);
-      if (num == 0){
-        for(int i = 0; i < 2048; i++){
-          printf("Bloque %d   =  ", i+1);
-          printBinary(bitMap -> Shells[i]);
-        }
-      }
-      else{
-        printf("Bloque %d   =  ", num);
-        printBinary(bitMap -> Shells[num -1]);
-      }
+      int num = atoi(input[1]);
+      os_bitmap(num);
 
     }
 
-    iter = 0;  
-    
+    else if (strncmp(input[0],"os_lifemap", 15) == 0){
+      printf("Entering OS_LIFEMAP \n");
+      //Se asume que ya se montó el disco
+      int lower = atoi(input[1]);
+      int upper = atoi(input[2]);
+      os_lifemap(lower, upper);
+    }
+
+    else if (strncmp(input[0],"os_exit", 15) == 0){
+      iter = 0;  
+    }
+
+    free_user_input(input);
   }
 
-  
- 
-  
-    
 }
 
 
--- src/ssdfs/os_API.c
+++ src/ssdfs/os_API.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
+FILE *ptr;
+unsigned int life;
+char* diskName;
 
 Page* pageInit(){
     Page* newPage = calloc(1, sizeof(Page));
@@ -56,7 +57,74 @@ Page* chargeBitMap(FILE* ptr){
 //------------------------------------------------------------------//
 
 
-void os_mount(char* diskname, unsigned int life, FILE** ptr){
-    *ptr = fopen(diskname,"rb");  // r for read, b for binary
+void os_mount(char* diskname, unsigned int input_life){
+    ptr = fopen(diskname,"rb");  // r for read, b for binary
+    life = input_life;
+    diskName = diskname;
     //"simdiskfilled.bin"
+}
+
+void os_bitmap(unsigned int num){
+    Page* bitMap = pageInit();
+    printf("%s AAAAAAAA\n", diskName);
+    seekPage(0, 0, ptr, bitMap ->Shells);
+    printf("AAAAAAAA\n");
+    if (num == 0){
+      for(int i = 0; i < 2048; i++){
+        printf("Celda %d   =  ", i+1);
+        printBinary(bitMap -> Shells[i]);
+      }
+    }
+    else{
+      printf("Celda %d   =  ", num);
+      printBinary(bitMap -> Shells[num -1]);
+    }
+
+    free(bitMap -> Shells);
+    free(bitMap);
+}
+
+void os_lifemap(int lower, int upper){
+    int current_page = lower;
+    int last_page = upper;
+    if (lower == -1 && upper == -1){
+      current_page = 0;
+      last_page = 255;
+    }
+    Block* lifeMapBlock1 = blockInit();
+    Page* page;
+    for(current_page = current_page; current_page <= last_page; current_page++){
+        page = &(lifeMapBlock1 -> Pages[current_page]);
+        seekPage(1, current_page, ptr, page ->Shells);
+        for(int i = 0; i < 2048; i++){
+          printf("Bloque 1, Pagina %d, Celda %d   =  ", current_page+1, i+1);
+          printBinary(page -> Shells[i]);
+        }
+        free(page->Shells);
+    }
+
+    free(lifeMapBlock1->Pages);
+    free(lifeMapBlock1);
+
+    current_page = lower;
+    last_page = upper;
+    if (lower == -1 && upper == -1){
+      current_page = 0;
+      last_page = 255;
+    }
+    Block* lifeMapBlock2 = blockInit();
+    printf("BLOQUE 2\n");
+    for(current_page = current_page; current_page <= last_page; current_page++){
+        page = &(lifeMapBlock2 -> Pages[current_page]);
+        seekPage(2, current_page, ptr, page ->Shells);
+        for(int i = 0; i < 2048; i++){
+          printf("Bloque 2, Pagina %d, Celda %d   =  ", current_page+1, i+1);
+          printBinary(page -> Shells[i]);
+        }
+        free(page->Shells);
+    }
+
+    free(lifeMapBlock2->Pages);
+    free(lifeMapBlock2);
+
 }
